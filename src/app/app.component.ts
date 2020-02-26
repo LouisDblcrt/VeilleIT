7fef96d3a0bd77fff484fec82ef13179493e1dc1
Louis DOUBLECOURT <louis.dbl@hotmail.fr>
2020-02-26 18:48:57 +0100

    pb cors policy a regler @LFS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpHeaders } from "@angular/common/http";
 import * as xml2js from "xml2js";
 import { NewsRss } from './news-rss';
 
@@ -10,15 +10,18 @@ import { NewsRss } from './news-rss';
 })
 export class AppComponent {
   title = 'VeilleIT';
+  access_token="6a0a149b-f958-0a5a-507d-0b9c66";
+  consumer_key="90029-6e5808843cc3bdcdafe7a477"
   RssData: NewsRss;
   constructor(private http: HttpClient) {}
   GetRssFeedData() {
-    const requestOptions: Object = {
-      observe: "body",
-      responseType: "text"
+    const optionRequete = {
+      headers: new HttpHeaders({ 
+        'Access-Control-Allow-Origin':'*'
+      })
     };
     this.http
-      .get<any>("https://www.frandroid.com/feed", requestOptions)
+      .post<any>(`https://getpocket.com/v3/get?consumer_key=${this.consumer_key}&access_token=${this.access_token}`, optionRequete)
       .subscribe(data => {
         let parseString = xml2js.parseString;
         parseString(data, (err, result: NewsRss) => {
