b3ad0c37a302359f044660b79090f75aef736664
Lucas FRESIERS <fresiers.lucas@gmail.com>
2020-02-28 15:21:23 +0100

    ajout d'un peu de css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ListOfResumeComponent } from './list-of-resume/list-of-resume.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AddArticleComponent } from './add-article/add-article.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListOfResumeComponent
+    ListOfResumeComponent,
+    AddArticleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
